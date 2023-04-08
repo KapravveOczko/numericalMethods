113f45f5a76fc8213146d85e319810c0c09d5427
Kacper Czernik <57114894+KapravveOczko@users.noreply.github.com>
2023-04-08 17:49:24 +0200

    Zadanie3 | Polymonial works fine | searching for so called rownolegle rozlozenie

--- zadanie3/lagrange.py
+++ zadanie3/lagrange.py
@@ -1,13 +1,30 @@
-def LagrangePolynomial(x, n, i):
+def lagrangePolynomialinternal(x, n, i, tabX):
+    """
+    wyznacza wartość L (wielomian bazowy)
+    :param tabX: tablica wartości X
+    :param x: wartość zmiennej x
+    :param n: ilość iteracji
+    :param i: numer iteracji z wielomianu
+    :return: wartość L z uwzglęnieniem zmiennej x
+    """
     counter = 1
     denominator = 1
-    '''wyznaczenie wartości pojedyńczego L'''
-    for j in range(n+1):
+    for j in range(n):
         if j != i:
-            counter *= (x - j)
-            denominator *= (i - j)
+            counter *= (x - tabX[j])
+            denominator *= (tabX[i] - tabX[j])
     return (counter / denominator)
 
-def LagrangePolynomial():
-
-    return 0
+def lagrangePolynomial(x,n,tabX,tabY):
+    """
+    funkcja wyznacza wartość wielomianu interpolacyjnego
+    :param x: wartość zmiennej x
+    :param n: ilość iteracji
+    :param tabX:
+    :param tabY:
+    :return: wartość wielomianu interpolacyjnego dla zmiennej x
+    """
+    polymonial = 0
+    for i in range(n):
+        polymonial += tabY[i]*lagrangePolynomialinternal(x,n,i,tabX)
+    return polymonial
--- zadanie3/main.py
+++ zadanie3/main.py
@@ -1,1 +1,8 @@
+from lagrange import *
 print("hello world")
+
+#testowe wartości pobrane z https://pracownik.kul.pl/files/10382/public/aan_w2_1819_1.pdf
+testTabX = [0,1,3]
+testTabY = [1,3,2]
+x = 2
+print(lagrangePolynomial(x, 3,testTabX,testTabY))
