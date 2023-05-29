25cc38a5616b68547b96b95f9c8cc74f92d90449
Kacper Czernik <Kacper-czernik@wp.pl>
2023-05-29 22:49:22 +0200

    repo | did some cleaning

--- zadanie5/functions.py
+++ zadanie5/functions.py
@@ -5,18 +5,17 @@ def linealFunction(x):
 
 def absoluteFunction(x):
     x = abs(x)
-    return 5*x-8
-    # return x
+    # return 5*x-8
+    return x
 def polynomialFunction(x):
-    return 2*x**3-5*x**2-3*x+6
-    # return 2*x**2+x-2
+    # return 2*x**3-5*x**2-3*x+6
+    return 2*x**2+x-2
 def trigonometricFunction(x):
-    return -3*numpy.sin(x)+6*numpy.cos(x)
-    # return numpy.sin(x)
+    # return -3*numpy.sin(x)+6*numpy.cos(x)
+    return numpy.sin(x)
 def complexFunction(x):
-    return -2*numpy.cos(x)-3*x**2+x**3
-    # return numpy.cos(2*x**2+1)
-
+    # return -2*numpy.cos(x)-3*x**2+x**3
+    return numpy.cos(2*x**2+1)
 def horner(poly, x):
     result = poly[0]
     for i in range(1, len(poly)):
